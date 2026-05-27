c12ad7dcc9563f8d78584c18d5362f556a0ee53d
William Balaguera <Williamstivenbg@ufps.edu.co>
2026-05-27 08:29:26 -0500

    Initial commit

---
+++ src/main/java/com/universidad/productos_web/ProductosWebApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.productos_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosWebApplication.class, args);
+	}
+
+}
